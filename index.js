e5f14a29113ddc483328177c42f998ba5eebd723
fsbalbuena <fsbalbuena@gmail.com>
2019-05-06 02:01:16 -0300

    Arreglos basicos de redireccion

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
-var array=[
-{id:"#site",url:"#"},
-{id:"#tr",url:"#"},
+
+var array=[{id:"#ce",url:"#pepe"},
+{id:"#bfe",url:"https://www.felizemprendiendo.com"},
+{id:"#tr",url:"https://demos.desarrollospyme.com/pw-trabajos.html"},
 {id:"#nos-l",url:"#nos"},
-{id:"#design",url:"#"},
+{id:"#design",url:"https://demos.desarrollospyme.com/pw-trabajos.html?d=2"},
 {id:"#face",url:"#"},
-{id:"#insta",url:"#"}]
+{id:"#insta",url:"#"},
+,{id:"#pwi",url:"https://www.desarrollospyme.com/paginas-web#Paginas-institucionales"},
+{id:"#pw24",url:"https://www.desarrollospyme.com/paginas-web#Paginas-24hs"},
+{id:"#gM",url:"https://www.desarrollospyme.com/paginas-web#GoogleMaps"},
+{id:"#price",url:"https://www.desarrollospyme.com/paginas-web#Precios"}]
 
 array.forEach(element => {
     
